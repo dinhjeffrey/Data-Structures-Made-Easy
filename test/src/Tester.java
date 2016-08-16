695856866329806d4db642a53587a0ae98fd17d7
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-08-16 09:57:53 -0700

    added immutable strings

--- test/src/Tester.java
+++ test/src/Tester.java
@@ -2,5 +2,9 @@ public class Tester {
     public static void main(String[] args) {
         Person p = new Student("Jose", 12345);
         System.out.println(p);
+
+        String canada = "lala";
+        String toronto = canada.concat("ortho");
+        System.out.println(toronto);
     }
 }
